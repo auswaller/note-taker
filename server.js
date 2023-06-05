5450c253f80d5344974a274d14d700b28c33cbbf
auswaller <auswaller@gmail.com>
2023-06-05 09:51:16 -0400

    added init notes.js route

--- server.js
+++ server.js
@@ -13,16 +13,16 @@ app.use('/api', api);
 
 app.use(express.static('public'));
 
-// GET Route for homepage
+//Route for homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for feedback page
+//Route for notes 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
 );
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+  console.log(`App listening at http://localhost:${PORT}`)
 );
