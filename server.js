1a89aee6cb9536da064fb66884617b806786286a
auswaller <auswaller@gmail.com>
2023-06-05 09:49:19 -0400

    modified PORT for Heroku, fixed get for /notes

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const api = require('./routes/index.js');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
@@ -19,8 +19,8 @@ app.get('/', (req, res) =>
 );
 
 // GET Route for feedback page
-app.get('/feedback', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
 );
 
 app.listen(PORT, () =>
